84bf41f187ed7d0e964363f2ef519984fd920297
kazuya <kenhks2004@gmail.com>
2022-08-17 22:33:43 +0800

    Init project

---
+++ src/main/java/com/example/pmss/PmssApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmssApplication.class, args);
+	}
+
+}
